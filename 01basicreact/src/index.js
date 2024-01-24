c96878b90aeba97ef1f4b6b53e046648bcc69657
khushboosinghal25 <khushboosinghal25092004@gmail.com>
2024-01-24 13:37:58 +0530

    day2

---
+++ 01basicreact/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// import chaihai from '../../01vitereact/src/chai';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App/>
+);
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+// import chai from "./chai"/
+import Chai from './chai'
+
+function App() {
+
+  return (
+    <Chai/>
+  )
+}
+
+export default App
