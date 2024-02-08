7d09b370693ce67970a5d8f2ff4b7ad57937e60b
khushboosinghal25 <khushboosinghal25092004@gmail.com>
2024-02-08 17:55:41 +0530

    gitignore has been added

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState , useCallback , useRef , useEffect } from 'react'
+// useEffect , useRef , useCallback
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length , setLength] = useState(8);
+  const [char , setchar] = useState(false);
+  const [num , setnum] = useState(false);
+  const [Password,setPassword] = useState("");
+  const passwordGenerator = useCallback(fn , [length , num , char , setPassword])
+
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='w-400 , h-100 bg-slate-700'>
+      <h1 className='text-center text-4xl text-yellow-400 m-4 p-4'>Password Generator</h1>
+
+    </div>
     </>
   )
 }
